afbf9fa038c58f2e4b23e7e6e025f396294afd5f
Vadym Frolov <vdfroloff@gmail.com>
2023-08-01 17:21:45 +0300

    WORK:
    - routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
-export function App() {
-  return <p>Here to start</p>;
-}
+import { lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+// import { AppBar } from 'components/AppBar/AppBar';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const EventCreatePage = lazy(() => import('pages/EventCreatePage'));
+const EventDetailsPage = lazy(() => import('pages/EventDetailsPage'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}>
+        <Route index element={<HomePage />} />
+        <Route path="create" element={<EventCreatePage />} />
+        <Route path="event/:eventId" element={<EventDetailsPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
+};
