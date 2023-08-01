b8fa0ddfa08d425129a22d0f942603bebbaadd88
Vadym Frolov <vdfroloff@gmail.com>
2023-08-01 23:55:10 +0300

    WORK:
    - move fonts to assets folder
    - fonst.ts small update of path, etc

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins-Light'),
-        url(src/fonts/Poppins-Light.ttf) format('truetype');
+        url("src/fonts/Poppins-Light.ttf") format('truetype');
     font-weight: 300;
     font-style: normal;
 }
@@ -12,7 +12,7 @@ export const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins-Regular'),
-        url(src/fonts/Poppins-Regular.ttf) format('truetype');
+        url("src/assets/fonts/Poppins-Regular.ttf") format('truetype');
     font-weight: 400;
     font-style: normal;
 }
@@ -20,7 +20,7 @@ export const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins-Medium'),
-        url("src/fonts/Poppins-Medium.ttf") format('truetype');
+        url("src/assets/fonts/Poppins-Medium.ttf") format('truetype');
     font-weight: 500;
     font-style: normal;
 }
@@ -28,14 +28,14 @@ export const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins-SemiBold'),
-        url(src/fonts/Poppins-SemiBold.ttf) format('truetype');
+        url("src/assets/fonts/Poppins-SemiBold.ttf") format('truetype');
     font-weight: 600;
     font-style: normal;
 }
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins-Bold'),
-        url(src/fonts/Poppins-Bold.ttf) format('truetype');
+        url("src/assets/fonts/Poppins-Bold.ttf") format('truetype');
     font-weight: 700;
     font-style: normal;
 }
@@ -43,7 +43,7 @@ export const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: 'Poppins';
     src: local('Poppins-ExtraBold'),
-        url(src/fonts/Poppins-ExtraBold.ttf) format('truetype');
+        url("src/assets/fonts/Poppins-ExtraBold.ttf") format('truetype');
     font-weight: 800;
     font-style: normal;
 }
