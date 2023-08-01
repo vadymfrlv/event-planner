e0fe070c7cea291ac0ae380ad392dceb5fb46f20
Vadym Frolov <vdfroloff@gmail.com>
2023-08-01 16:37:45 +0300

    WORK:
    - theme draft added

---
+++ src/utils/theme.ts
@@ -0,0 +1,42 @@
+export const theme = {
+  colors: {
+    white: '#fff',
+    main: '#1900ff',
+    accent: '#e7fc00',
+    hoverBackground: '#e7fc008d',
+  },
+  fontSizes: [14, 15, 16, 18, 20, 25, 30],
+  fontWeights: {
+    regular: 400,
+    semiMedium: '500',
+    medium: 600,
+    bold: 700,
+    black: 900,
+  },
+  letterSpacings: {
+    small: '2px',
+    normal: '3px',
+    wide: '10px',
+  },
+  lineHeights: {
+    body: 1.3,
+  },
+  borders: {
+    none: 'none',
+    headerFooter: '6px solid #e7fc00',
+    btn: '3px solid #e7fc00',
+    decorative: '2px dashed #fff',
+  },
+  radii: {
+    none: '0',
+    btn: '4px',
+    poster: '5px',
+  },
+  shadows: {
+    card: '0px 12px 29px -1px rgba(247, 255, 0, 0.77)',
+    navLink:
+      '0 0 3px #91ff00, 0 0 8px #91ff00, 0 0 13px #91ff00, 0 0 17px #91ff00, 0 0 22px #91ff00, 0 0 27px #91ff00, 0 0 32px #91ff00',
+    btn: '0 0 5px #91ff00, 0 0 5px #91ff00, 0 0 9px #91ff00, 0 0 13px #91ff00, 0 0 18px #91ff00, 0 0 23px #91ff00, 0 0 28px #91ff00',
+  },
+  zIndices: [-1, 1],
+};
