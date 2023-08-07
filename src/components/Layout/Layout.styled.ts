3a02bf7f1af30a2d40084b59306ba6c8a49988d3
Vadym Frolov <vdfroloff@gmail.com>
2023-08-07 21:58:51 +0300

    WORK:
    - background responsive done

--- src/components/Layout/Layout.styled.ts
+++ src/components/Layout/Layout.styled.ts
@@ -8,7 +8,6 @@ export const MainStyled = styled.main`
   padding-bottom: 42px;
   background-image: url(${mainBackground});
   background-repeat: no-repeat;
-  /* background-size: 1494px 1639px; */
   background-position: top -3px left 35%;
 
   @media (min-width: ${p => p.theme.screens.tab}) {
@@ -22,34 +21,3 @@ export const MainStyled = styled.main`
     background-position: top -3px left 97%;
   }
 `;
-
-// export const MainStyled = styled.main`
-//   position: relative;
-//   height: 100vh;
-//   padding-top: 40px;
-//   padding-bottom: 42px;
-//   /* overflow: hidden; */
-//   z-index: 5;
-
-//   @media (min-width: ${p => p.theme.screens.tab}) {
-//     padding-bottom: 125px;
-//   }
-
-//   @media (min-width: ${p => p.theme.screens.desk}) {
-//     padding-top: 60px;
-//     padding-bottom: 77px;
-//   }
-
-//   &::before {
-//     content: '';
-//     position: absolute;
-//     top: -3px;
-//     left: -30px;
-//     min-height: 180%;
-//     min-width: 180%;
-//     background-image: url(${mainBackground});
-//     background-repeat: no-repeat;
-//     background-size: cover;
-//     background-position: top -3px left 45%;
-//   }
-// `;
