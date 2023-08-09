e3cece76c0515dfe2e0efba7c2526bd98c996531
Vadym Frolov <vdfroloff@gmail.com>
2023-08-09 21:34:31 +0300

    WORK:
    - changes to responsive mode to correctly display header elements under the mockup

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -15,6 +15,18 @@ export const HeaderStyled = styled.header`
 export const LogoLinkStyled = styled(Link)`
   font-family: ${p => p.theme.fonts.logo};
   font-size: ${p => p.theme.fontSizes[0]}px;
+  margin-right: auto;
 
   color: ${p => p.theme.colors.accent};
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  align-items: center;
+
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    flex-wrap: nowrap;
+  }
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,18 @@
-import { BaseBox, ResponsiveBox } from 'components/Box/Box.jsx';
+import { ResponsiveBox } from 'components/Box/Box.jsx';
 import { LanguageSelector } from 'components/LanguageSelector/LanguageSelector';
 import { SearchBox } from 'components/SearchBox/SearchBox';
 
-import { HeaderStyled, LogoLinkStyled } from './Header.styled';
+import { HeaderStyled, LogoLinkStyled, Wrapper } from './Header.styled';
 
 export const Header = () => {
   return (
     <HeaderStyled>
       <ResponsiveBox display="flex" alignItems="center" justifyContent="space-between">
-        <LogoLinkStyled to={'/'}>Event Planner</LogoLinkStyled>
-        <BaseBox display="flex" alignItems="center">
+        <Wrapper>
+          <LogoLinkStyled to={'/'}>Event Planner</LogoLinkStyled>
           <SearchBox />
           <LanguageSelector />
-        </BaseBox>
+        </Wrapper>
       </ResponsiveBox>
     </HeaderStyled>
   );
