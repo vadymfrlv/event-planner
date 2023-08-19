334ae10e34645106190b3e91508895b69721a652
Vadym Frolov <vdfroloff@gmail.com>
2023-08-19 22:27:34 +0300

    WORK:
    - ActionBtn updated and might be done

--- src/components/ActionBtn/ActionBtn.styled.tsx
+++ src/components/ActionBtn/ActionBtn.styled.tsx
@@ -1,45 +1,62 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
+import { LuPlus } from 'react-icons/lu';
 
-interface ActionBtnProps {
-  children: string;
-  isLink: boolean;
-  to?: string; // Опційний параметр, але потрібно явно вказати тип
-  type?: 'button' | 'reset' | 'submit';
-}
+const sharedStyles = css`
+  padding: 16px;
+  color: ${p => p.theme.colors.white};
+  background-color: ${p => p.theme.colors.accent};
+  font-family: 'Poppins';
+  font-weight: 500;
+  border-radius: 8px;
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    padding: 16px 12px;
+  }
+`;
 
-const ActionBtnBase = styled.button`
-  display: block;
-  padding: 10px;
-  background-color: red;
-  border: none;
+export const AddIcon = styled(LuPlus)`
+  margin-right: 0;
+  height: 24px;
+  width: 24px;
+  color: ${p => p.theme.colors.white};
 
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    margin-right: 16px;
+  }
+`;
+
+export const ActionLinkStyled = styled(Link)`
+  ${sharedStyles}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+
+  font-size: 16px;
+  box-shadow: 2px 4px 9px 0px rgba(166, 141, 174, 0.28);
+  transition: background-color 250ms ease-in-out;
+
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    width: 169px;
+  }
   &:hover {
-    background-color: green;
+    background-color: ${p => p.theme.colors.btnHover};
   }
-  /* Додайте ваші спільні стилі тут */
 `;
 
-const ActionLinkBase = styled(Link)`
-  display: block;
-  /* Додайте ваші спільні стилі тут */
+export const ActionBtnStyled = styled.button`
+  ${sharedStyles}
+
+  font-size: 16px;
+
+  border: none;
 `;
 
-export const ActionBtnStyled = ({ isLink, ...props }: ActionBtnProps) => {
-  if (isLink) {
-    const { to, children, ...linkProps } = props;
-    return (
-      <ActionLinkBase to={to || ''} {...linkProps}>
-        {children}
-      </ActionLinkBase>
-    );
-  } else {
-    const { type, children, ...buttonProps } = props;
-    return (
-      <ActionBtnBase type={type} {...buttonProps}>
-        {children}
-      </ActionBtnBase>
-    );
+export const DescriptionStyled = styled.span`
+  display: none;
+
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    display: block;
   }
-};
+`;
--- src/components/ActionBtn/ActionBtn.tsx
+++ src/components/ActionBtn/ActionBtn.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-
-import { ActionBtnStyled } from './ActionBtn.styled';
+import { AddIcon, ActionLinkStyled, ActionBtnStyled, DescriptionStyled } from './ActionBtn.styled';
 
 interface ActionBtnProps {
-  children: string;
+  isLink: boolean;
+  description?: string;
+  to?: string;
+  type?: 'button' | 'reset' | 'submit';
 }
 
-export const ActionBtn = ({ children }: ActionBtnProps) => {
-  return (
-    <>
-      <ActionBtnStyled isLink to="/">
-        {children}
+export const ActionBtn = ({ isLink, description, to, type, ...props }: ActionBtnProps) => {
+  if (isLink) {
+    return (
+      <ActionLinkStyled to={to || ''} {...props}>
+        <AddIcon />
+        <DescriptionStyled>{description}</DescriptionStyled>
+      </ActionLinkStyled>
+    );
+  } else {
+    return (
+      <ActionBtnStyled type={type} {...props}>
+        {description}
       </ActionBtnStyled>
-      <ActionBtnStyled isLink={false} type="submit">
-        {children}
-      </ActionBtnStyled>
-    </>
-  );
+    );
+  }
 };
