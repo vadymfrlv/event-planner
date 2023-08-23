9b36f3b4139aebf0eeae0f823ac0599f90a80a84
Vadym Frolov <vdfroloff@gmail.com>
2023-08-23 22:13:19 +0300

    WORK:
    - filter options constant created

---
+++ src/constants/filterOptions.ts
@@ -0,0 +1,9 @@
+export const filterOptions = [
+  { value: 'art', label: 'Art' },
+  { value: 'music', label: 'Music' },
+  { value: 'business', label: 'Business' },
+  { value: 'conference', label: 'Conference' },
+  { value: 'workshop', label: 'Workshop' },
+  { value: 'party', label: 'Party' },
+  { value: 'sport', label: 'Sport' },
+];
