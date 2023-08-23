d60c70619d1717b2c736f586dc9069888c5fbe05
Vadym Frolov <vdfroloff@gmail.com>
2023-08-23 22:11:38 +0300

    WORK:
    - sort options constant created

---
+++ src/constants/sortOptions.ts
@@ -0,0 +1,8 @@
+export const sortOptions = [
+  { value: 'name', label: 'by name', type: 'inc', key: 'A-Z' },
+  { value: 'name', label: 'by name', type: 'dec', key: 'Z-A' },
+  { value: 'date', label: 'by date', type: 'inc', key: 'date' },
+  { value: 'date', label: 'by date', type: 'dec', key: 'date' },
+  { value: 'priority', label: 'by priority', type: 'inc', key: 'priority' },
+  { value: 'priority', label: 'by priority', type: 'dec', key: 'priority' },
+];
