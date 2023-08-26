366efc14094b2d712ebdf7c7627ac292c7a1ca39
Vadym Frolov <vdfroloff@gmail.com>
2023-08-26 23:23:37 +0300

    WORK:
    - small upd due to components' name change

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
 import { PageTitle } from 'components/PageTitle/PageTitle';
 import { ActionBtn } from 'components/ActionBtn/ActionBtn';
 import { BaseBox } from 'components/Box/Box';
-import { FilterBy } from 'components/Filter/Filter';
-import { SortBy } from 'components/Sorter/Sorter';
+import { Filter } from 'components/Filter/Filter';
+import { Sorter } from 'components/Sorter/Sorter';
 
 const HomePage = () => {
   return (
     <BaseBox display="flex" alignItems="center">
       <PageTitle title="My events" />
-      <FilterBy />
-      <SortBy />
+      <Filter />
+      <Sorter />
       <ActionBtn description="Add new event" isLink to="createEvent" />
     </BaseBox>
   );
