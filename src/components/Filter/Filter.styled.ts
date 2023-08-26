04eef7e2fc0a492992f363a4916e4a0f8e21b298
Vadym Frolov <vdfroloff@gmail.com>
2023-08-26 23:22:47 +0300

    WORK:
    - logic and style upds for Filter

--- src/components/Filter/Filter.styled.ts
+++ src/components/Filter/Filter.styled.ts
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { LuFilter } from 'react-icons/lu';
 
-interface ILanguageOptionsProps {
-  $languageOptions: boolean;
+interface FilterProps {
+  $filterOptions?: boolean;
+  $filterSelected?: boolean;
+  $active?: boolean;
 }
 
-export const FilterByContainer = styled.div`
+export const FilterContainer = styled.div`
   position: relative;
   display: inline-block;
 
@@ -14,11 +16,11 @@ export const FilterByContainer = styled.div`
   }
 `;
 
-export const FilterSelectorButton = styled.button<ILanguageOptionsProps>`
+export const FilterSelectorButton = styled.button<FilterProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 154px;
+  width: ${p => (p.$filterOptions ? '154px' : '56px')};
   height: 56px;
   padding: 16px;
   font-family: ${p => p.theme.fonts.main};
@@ -26,48 +28,63 @@ export const FilterSelectorButton = styled.button<ILanguageOptionsProps>`
   font-size: 16px;
   border: none;
 
-  color: ${p => p.theme.colors.text};
+  color: ${p => (p.$filterSelected ? p.theme.colors.accent : p.theme.colors.text)};
 
   background-color: ${p => p.theme.colors.white};
   border-radius: ${p => p.theme.radii.eight};
   box-shadow: ${p => p.theme.shadows.elements};
   cursor: pointer;
+
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    width: 154px;
+  }
+
+  &:hover {
+    color: ${p => p.theme.colors.accent};
+  }
 `;
 
-export const FilterByIconStyled = styled(LuFilter)`
+export const FilterDescription = styled.span<FilterProps>`
+  display: ${p => (p.$filterOptions ? 'block' : 'none')};
+
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    display: block;
+  }
+`;
+
+export const FilterIconStyled = styled(LuFilter)`
   width: 24px;
   height: 24px;
-  color: ${p => p.theme.colors.text};
 `;
 
-export const FilterByOptions = styled.ul<ILanguageOptionsProps>`
+export const FilterOptions = styled.ul<FilterProps>`
   position: absolute;
-  display: ${p => (p.$languageOptions ? 'block' : 'none')};
-  top: ${p => (p.$languageOptions ? '63px' : '48px')};
+  visibility: ${p => (p.$filterOptions ? 'visible' : 'hidden')};
+  top: ${p => (p.$filterOptions ? '63px' : '48px')};
   left: 0;
   margin: 0 auto;
   width: 154px;
   background-color: ${p => p.theme.colors.white};
   border-radius: ${p => p.theme.radii.eight};
   box-shadow: ${p => p.theme.shadows.elements};
-  opacity: ${p => (p.$languageOptions ? 1 : 0)};
+  opacity: ${p => (p.$filterOptions ? 1 : 0)};
   transition: top 250ms ease-in-out, opacity 300ms ease-in-out;
 `;
 
-export const FilterByOption = styled.li`
+export const FilterOption = styled.li<FilterProps>`
   font-size: 14px;
   line-height: 20px;
+  color: ${p => (p.$active ? p.theme.colors.accent : p.theme.colors.divider)};
   cursor: pointer;
-  color: ${p => p.theme.colors.divider};
   font-weight: ${p => p.theme.fontWeights.medium};
   padding: 8px 24px;
 
   &:not(:last-child) {
-    border-bottom: ${p => p.theme.borders.listDivider};
+    border-bottom: ${p =>
+      p.$active ? p.theme.borders.listDividerActive : p.theme.borders.listDivider};
   }
 
-  &:hover,
-  &:focus {
+  &:hover {
     color: ${p => p.theme.colors.accent};
 
     &:not(:last-child) {
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -4,50 +4,66 @@ import { filterOptions } from 'constants/filterOptions';
 import { ClickOutsideWrapper } from 'components/ClickOutsideWrapper/ClickOutsideWrapper';
 
 import {
-  FilterByContainer,
+  FilterContainer,
   FilterSelectorButton,
-  FilterByOptions,
-  FilterByOption,
-  FilterByIconStyled,
+  FilterDescription,
+  FilterOptions,
+  FilterOption,
+  FilterIconStyled,
 } from './Filter.styled';
 
-export const FilterBy = () => {
-  const [currentFilterBy, setCurrentFilterBy] = useState('');
-  const [isFilterByDropdownOpen, setIsFilterByDropdownOpen] = useState(false);
+export const Filter = () => {
+  const [currentFilter, setCurrentFilter] = useState('');
+  const [isFilterDropdownOpen, setIsFilterDropdownOpen] = useState(false);
 
-  const toggleFilterByDropdown = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    setIsFilterByDropdownOpen(!isFilterByDropdownOpen);
+  const handleFilterChange = (filterOption: string) => {
+    const transformedFilterOption = filterOption.charAt(0).toUpperCase() + filterOption.slice(1);
+
+    if (currentFilter === transformedFilterOption) {
+      setCurrentFilter('');
+      setIsFilterDropdownOpen(false);
+      return;
+    }
+
+    setCurrentFilter(transformedFilterOption);
+    setIsFilterDropdownOpen(false);
   };
 
-  const handleFilterByChange = (filterByOption: string) => {
-    setCurrentFilterBy(filterByOption);
-    setIsFilterByDropdownOpen(false);
+  const toggleFilterDropdown = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsFilterDropdownOpen(!isFilterDropdownOpen);
   };
 
   const handleOutsideClick = () => {
-    setIsFilterByDropdownOpen(false);
+    setIsFilterDropdownOpen(false);
   };
 
   return (
     <ClickOutsideWrapper onClickOutside={handleOutsideClick}>
-      <FilterByContainer>
+      <FilterContainer>
         <FilterSelectorButton
-          $languageOptions={isFilterByDropdownOpen}
-          onClick={toggleFilterByDropdown}
+          $filterOptions={isFilterDropdownOpen}
+          $filterSelected={!!currentFilter}
+          onClick={toggleFilterDropdown}
         >
-          {currentFilterBy || 'Category'}
-          <FilterByIconStyled />
+          <FilterDescription $filterOptions={isFilterDropdownOpen}>
+            {currentFilter || 'Category'}
+          </FilterDescription>
+          <FilterIconStyled />
         </FilterSelectorButton>
 
-        <FilterByOptions $languageOptions={isFilterByDropdownOpen}>
+        <FilterOptions $filterOptions={isFilterDropdownOpen}>
           {filterOptions.map(({ value, label }) => (
-            <FilterByOption key={value} onClick={() => handleFilterByChange(label)}>
+            <FilterOption
+              key={value}
+              $active={currentFilter === label}
+              onClick={() => handleFilterChange(value)}
+            >
               {label}
-            </FilterByOption>
+            </FilterOption>
           ))}
-        </FilterByOptions>
-      </FilterByContainer>
+        </FilterOptions>
+      </FilterContainer>
     </ClickOutsideWrapper>
   );
 };
