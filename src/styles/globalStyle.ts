2dc081152781afed13f53ec5e3c06a83fbe16a1a
Vadym Frolov <vdfroloff@gmail.com>
2023-10-12 23:03:26 +0300

    WORK:
    - commented global style box-sizing

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -4,6 +4,10 @@ import './fonts.css';
 
 export const GlobalStyle = createGlobalStyle`
 
+/* * {
+  box-sizing: border-box;
+} */
+
 body {
   margin: 0;
   font-family: 'Poppins', 'Alata', sans-serif;
