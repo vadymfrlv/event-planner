f85cb211a01d076170f99b1886737292ce97cba9
Vadym Frolov <vdfroloff@gmail.com>
2023-10-13 22:23:40 +0300

    WORK:
    - TanStack Query installation and config

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, StyleSheetManager } from 'styled-components';
 import isPropValid from '@emotion/is-prop-valid';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n.ts';
 
@@ -12,17 +14,22 @@ import 'styles/reset.css';
 import { theme } from 'utils/theme';
 import { GlobalStyle } from 'styles/globalStyle.ts';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <I18nextProvider i18n={i18n}>
-        <ThemeProvider theme={theme}>
-          <StyleSheetManager shouldForwardProp={isPropValid}>
-            <GlobalStyle />
-            <App />
-          </StyleSheetManager>
-        </ThemeProvider>
-      </I18nextProvider>
+      <QueryClientProvider client={queryClient}>
+        <I18nextProvider i18n={i18n}>
+          <ThemeProvider theme={theme}>
+            <StyleSheetManager shouldForwardProp={isPropValid}>
+              <GlobalStyle />
+              <App />
+            </StyleSheetManager>
+          </ThemeProvider>
+        </I18nextProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
