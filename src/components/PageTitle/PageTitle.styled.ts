467e8c6b2a20c5f9a4b814f56d44c75f75251618
Vadym Frolov <vdfroloff@gmail.com>
2023-10-13 21:07:04 +0300

    WORK:
    - adjusted style for breakpoints

--- src/components/PageTitle/PageTitle.styled.ts
+++ src/components/PageTitle/PageTitle.styled.ts
@@ -3,14 +3,22 @@ import styled from 'styled-components';
 export const PageTitleStyled = styled.h1`
   display: none;
 
+  margin-right: auto;
+
+  font-family: Poppins;
+  font-weight: 600;
+  font-size: 32px;
+
+  color: #3f3f3f;
+
   @media (min-width: ${p => p.theme.screens.tab}) {
     display: block;
-    margin-right: auto;
+    margin-top: 40px;
+  }
 
-    font-family: Poppins;
-    font-weight: 600;
-    font-size: 32px;
-
-    color: #3f3f3f;
+  @media (min-width: ${p => p.theme.screens.note}) {
+    display: block;
+    margin-top: 0;
+    order: -1;
   }
 `;
