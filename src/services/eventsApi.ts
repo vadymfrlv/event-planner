f054834ee83f18017735773cc898ab1de8a82cb3
Vadym Frolov <vdfroloff@gmail.com>
2023-10-14 19:43:19 +0300

    WORK:
    - added fetch fun to get all events

---
+++ src/services/eventsApi.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse } from 'axios';
+
+import { Event } from 'types/event';
+
+axios.defaults.baseURL = 'https://6527bc53931d71583df149e3.mockapi.io';
+
+export const fetchEvents = async (): Promise<Event[]> => {
+  try {
+    const response: AxiosResponse<Event[]> = await axios('/events');
+    if (response.status !== 200) {
+      throw new Error('Oops something went wrong!');
+    }
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
