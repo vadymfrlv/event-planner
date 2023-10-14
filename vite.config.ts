990725fa757cd83b00f05caece821d2a3062c03c
Vadym Frolov <vdfroloff@gmail.com>
2023-10-14 15:38:01 +0300

    WORK:
    - path alias added to configs

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,14 @@ export default defineConfig({
   resolve: {
     alias: {
       components: '/src/components',
+      assets: '/src/assets',
       pages: '/src/pages',
       services: '/src/services',
       utils: '/src/utils',
       styles: '/src/styles',
       constants: '/src/constants',
       hooks: '/src/hooks',
+      types: '/src/types',
     },
   },
 });
