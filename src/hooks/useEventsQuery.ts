93ccd28dfd797bfff48466cac3c580fdee7e7369
Vadym Frolov <vdfroloff@gmail.com>
2023-10-14 19:44:14 +0300

    WORK:
    - created a hook to get all events using tanstack query

---
+++ src/hooks/useEventsQuery.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchEvents } from 'services/eventsApi';
+
+export const useEventsQuery = () => {
+  return useQuery({
+    queryFn: () => fetchEvents(),
+    queryKey: ['events'],
+    staleTime: 1000 * 5,
+    onError: error => {
+      if (error instanceof Error) {
+        console.error(error);
+      }
+    },
+  });
+};
