c8ff1654a1668cbeab723dd2ed7d6ba5c4cf1a57
Vadym Frolov <vdfroloff@gmail.com>
2023-10-19 22:42:50 +0300

    WORK:
    - removed code than unneeded anymore
    - added Event component

--- src/pages/EventDetailsPage.tsx
+++ src/pages/EventDetailsPage.tsx
@@ -1,15 +1,11 @@
 import { BackButton } from 'components/BackButton/BackButton';
-import { useEventByIdQuery } from 'utils/hooks/useEventByIdQuery';
-// import { useParams } from 'react-router-dom';
+import { Event } from 'components/Event/Event';
 
 const EventDetailsPage = () => {
-  // const { eventId } = useParams();
-  const { data: event, isSuccess } = useEventByIdQuery();
-  console.log(event);
-
   return (
     <>
       <BackButton />
+      <Event />
     </>
   );
 };
