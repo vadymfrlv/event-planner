76a656bac101b85241a5cfee72d651ef00f54795
Vadym Frolov <vdfroloff@gmail.com>
2023-10-20 22:32:34 +0300

    WORK:
    - working on Event component

--- src/components/Event/Event.styled.ts
+++ src/components/Event/Event.styled.ts
@@ -2,4 +2,59 @@ import styled from 'styled-components';
 
 export const EventWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 24px;
+
+  @media (min-width: ${p => p.theme.screens.note}) {
+    padding-top: 17px;
+  }
+`;
+
+export const Title = styled.h2`
+  color: ${p => p.theme.colors.text};
+  font-family: ${p => p.theme.fonts.main};
+  font-size: 24px;
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+  line-height: normal;
+
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    font-size: 32px;
+  }
+`;
+
+export const EventInfoThumb = styled.div`
+  padding: 10px;
+`;
+
+export const EventPicture = styled.img`
+  height: 168px;
+  width: 272px;
+
+  object-fit: cover;
+
+  @media (min-width: ${p => p.theme.screens.tab}) {
+    height: 272px;
+    width: 688px;
+  }
+
+  @media (min-width: ${p => p.theme.screens.note}) {
+    width: 628px;
+  }
+
+  @media (min-width: ${p => p.theme.screens.desk}) {
+    height: 348px;
+    width: 800px;
+  }
+`;
+
+export const EventDescr = styled.p`
+  padding-bottom: 16px;
+  height: 72px;
+  color: ${p => p.theme.colors.EventDescr};
+  font-family: ${p => p.theme.fonts.main};
+  font-size: 14px;
+
+  font-weight: ${p => p.theme.fontWeights.regular};
+  line-height: 20px;
 `;
--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -1,8 +1,34 @@
 import { useEventByIdQuery } from 'utils/hooks/useEventByIdQuery';
 
-import { EventWrapper } from './Event.styled';
+import { Loader } from 'components/Loader/Loader';
+import { ErrorNotification } from 'components/ErrorNotification/ErrorNotification';
+
+import defaultPicWide from 'assets/images/defaults/default-wide.png';
+
+import { EventWrapper, Title, EventPicture, EventInfoThumb, EventDescr } from './Event.styled';
 
 export const Event = () => {
-  const { data: event, isSuccess } = useEventByIdQuery();
-  return <EventWrapper>{isSuccess && event.title}</EventWrapper>;
+  const { data: event, isFetching, isSuccess, isError } = useEventByIdQuery();
+
+  if (!event) {
+    return;
+  }
+
+  const { id, title, description, time, date, location, category, picture, priority } = event;
+
+  return (
+    <>
+      {isFetching && <Loader />}
+      {isSuccess && (
+        <EventWrapper>
+          <Title>{title}</Title>
+          <EventInfoThumb>
+            <EventPicture src={picture ? picture : defaultPicWide} alt={category.label} />
+            <EventDescr>{description}</EventDescr>
+          </EventInfoThumb>
+        </EventWrapper>
+      )}
+      {isError && <ErrorNotification />}
+    </>
+  );
 };
