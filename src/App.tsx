8fb50439b40952f5c69401b8d1f0f23e93f4d4c9
Vadym Frolov <vdfroloff@gmail.com>
2023-10-24 23:20:51 +0300

    ADDED AND FIXED:
    - added new route for EventEditPage
    - fixed path for EventCreatePage

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import { Layout } from 'components/Layout/Layout';
 const HomePage = lazy(() => import('pages/HomePage'));
 const EventCreatePage = lazy(() => import('pages/EventCreatePage'));
 const EventDetailsPage = lazy(() => import('pages/EventDetailsPage'));
+const EventEditPage = lazy(() => import('pages/EventEditPage'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="createEvent" element={<EventCreatePage />} />
+        <Route path="create-event" element={<EventCreatePage />} />
         <Route path="event/:eventId" element={<EventDetailsPage />} />
+        <Route path="event-edit/:eventId" element={<EventEditPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
