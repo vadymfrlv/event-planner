6859020dde65eeb7961dc58e3097329c66a9565f
Vadym Frolov <vdfroloff@gmail.com>
2023-10-24 23:19:35 +0300

    ADDED:
    - EventEditPage

---
+++ src/pages/EventEditPage.tsx
@@ -0,0 +1,12 @@
+import { BackButton } from 'components/BackButton/BackButton';
+
+const EventEditPage = () => {
+  return (
+    <>
+      <BackButton />
+      <div>EventEditPage</div>
+    </>
+  );
+};
+
+export default EventEditPage;
