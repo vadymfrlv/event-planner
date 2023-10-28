0f4f174990207198dbbbe9a595aa23f8de40f836
Vadym Frolov <vdfroloff@gmail.com>
2023-10-28 19:34:44 +0300

    WORK:
    - some upd for the future use

--- src/components/ActionBtn/ActionBtn.tsx
+++ src/components/ActionBtn/ActionBtn.tsx
@@ -1,13 +1,14 @@
 import { AddIcon, ActionLinkStyled, ActionBtnStyled, DescriptionStyled } from './ActionBtn.styled';
 
 interface ActionBtnProps {
-  isLink: boolean;
+  isLink?: boolean;
   description?: string;
   to?: string;
   type?: 'button' | 'reset' | 'submit';
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const ActionBtn = ({ isLink, description, to, type, ...props }: ActionBtnProps) => {
+export const ActionBtn = ({ isLink, description, to, type, onClick, ...props }: ActionBtnProps) => {
   if (isLink) {
     return (
       <ActionLinkStyled to={to || ''} {...props}>
@@ -17,7 +18,7 @@ export const ActionBtn = ({ isLink, description, to, type, ...props }: ActionBtn
     );
   } else {
     return (
-      <ActionBtnStyled type={type} {...props}>
+      <ActionBtnStyled type={type} onClick={onClick} {...props}>
         {description}
       </ActionBtnStyled>
     );
