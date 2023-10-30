647ffc8b3636be0fa36241a3a67514d6f6269a8a
Vadym Frolov <vdfroloff@gmail.com>
2023-10-30 20:51:42 +0200

    Update BackButton.tsx

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -5,9 +5,6 @@ export const BackButton = () => {
   const location = useLocation();
   const backLinkHref = location?.state?.from ?? '/';
 
-  console.log('Current location:', location);
-  console.log('Back link:', backLinkHref);
-
   return (
     <StyledBackBtn to={backLinkHref}>
       <IconBack />
