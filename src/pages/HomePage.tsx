c8db4a9a267b995eee87c260107d1d150fe9dd7a
Vadym Frolov <vdfroloff@gmail.com>
2023-11-03 23:49:37 +0200

    ADDED:
    - added request to receive data and push it to components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,23 @@
-import { EventList } from 'components/EventList/EventList';
+import { useState } from 'react';
+
 import { PagePanel } from 'components/PagePanel/PagePanel';
+import { EventList } from 'components/EventList/EventList';
+import { Pagination } from 'components/Pagination/Pagination';
+import { useEventsWithPaginationQuery } from 'utils/hooks/useEventsWithPaginationQuery';
 
 const HomePage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [limitPerPage, setLimitPerPage] = useState(8);
+
+  const queryResults = useEventsWithPaginationQuery(currentPage, limitPerPage);
   return (
     <>
       <PagePanel />
-      <EventList />
+      <EventList {...queryResults} />
+      <Pagination
+        {...{ ...queryResults, currentPage, limitPerPage }}
+        onPageChange={page => setCurrentPage(page)}
+      />
     </>
   );
 };
