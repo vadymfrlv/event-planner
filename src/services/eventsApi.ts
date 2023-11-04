04b0d233e8d865a2003e6ed5dfa9235880e32dde
Vadym Frolov <vdfroloff@gmail.com>
2023-11-04 18:30:32 +0200

    FIXED:
    - put back fetch of all events

--- src/services/eventsApi.ts
+++ src/services/eventsApi.ts
@@ -4,16 +4,16 @@ import { Event, FetchEventsResult } from 'types/event';
 
 axios.defaults.baseURL = 'https://6527bc53931d71583df149e3.mockapi.io';
 
-// export const fetchEvents = async (): Promise<Event[]> => {
-//   try {
-//     const response: AxiosResponse<Event[]> = await axios('/events');
-//     const { data } = response;
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
+export const fetchEvents = async (): Promise<Event[]> => {
+  try {
+    const response: AxiosResponse<Event[]> = await axios('/events');
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 export const fetchEventsWithPagination = async (
   page: number,
